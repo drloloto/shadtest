f707f1e6a532ce749b5fc4701f83a578602bcc95
drloloto <sebastian.hess@pm.me>
2024-10-29 15:44:33 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
  import * as Drawer from "$lib/components/ui/drawer";
-  import {
-  Button
- } from "$lib/components/ui/button/index.js";
- import * as Dialog from "$lib/components/ui/dialog/index.js";
- let open = $state(false)
 
-</script>
+import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+
+ let open = $state(false)
+ let open2 = $state(false)
+ $inspect(open, 'Drop')
+ $inspect(open2, 'draw')
  
-<Drawer.Root  bind:open>
+ </script>
+
+
+ 
+
+ <div class="w-full flex justify-center items-center h-screen">
+<DropdownMenu.Root bind:open={open}>
+  <DropdownMenu.Trigger class="bg-gray-500 p-4 rounded-lg">Open</DropdownMenu.Trigger>
+  <DropdownMenu.Content interactOutsideBehavior="defer-otherwise-close">
+    <DropdownMenu.Group>
+      <DropdownMenu.GroupHeading>My Account</DropdownMenu.GroupHeading>
+      <DropdownMenu.Separator />
+   <DropdownMenu.Item onclick={() => open2 = true}>
+      Click me
+   </DropdownMenu.Item>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root></div>
+
+
+<Drawer.Root bind:open={open2} >
  <Drawer.Trigger ></Drawer.Trigger>
- <Drawer.Content>
+ <Drawer.Content interactOutsideBehavior="close" >
   <Drawer.Header>
    <Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
    <Drawer.Description>This action cannot be undone.</Drawer.Description>
@@ -20,23 +40,3 @@
   </Drawer.Footer>
  </Drawer.Content>
 </Drawer.Root>
-
- 
-<Dialog.Root >
- <Dialog.Trigger  class="bg-gray-400 rounded p-4 "
->Edit Profile</Dialog.Trigger
- >
- <Dialog.Content class="sm:max-w-[425px]">
-  <Dialog.Header>
-   <Dialog.Title>Edit profile</Dialog.Title>
-   <Dialog.Description>
-    Make changes to your profile here. Click save when you're done.
-   </Dialog.Description>
-  </Dialog.Header>
-  <div class="grid gap-4 py-4">
-    <Button onclick={() => open = true} class="text-right">Open Drawer</Button>
-  </div>
-  <Dialog.Footer>
-  </Dialog.Footer>
- </Dialog.Content>
-</Dialog.Root>
